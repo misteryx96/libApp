6ed65d3fd2376117eaf91ef08593c9fc6359e97c
Mladen Karic <misteryx96@yahoo.com>
2021-03-18 11:06:54 +0100

    LibApp.Data Book

---
+++ LibApp/LibApp.Data/Model/Domain/Book.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibApp.Data.Model.Domain
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Code { get; set; }
+        public DateTime CreatedTimeStamp { get; set; }
+        public DateTime ModifiedTimestamp { get; set; }
+        public string Title { get; set; }
+        public string Publisher { get; set; }
+        //TODO: Prolly needs table
+        public string Category { get; set; }
+        public string Edition { get; set; }
+        //TODO: Needs table
+        public int DepartmentId { get; set; }
+        public bool IsAvailable { get; set; }
+        public bool IsReserved { get; set; }
+        //TODO: Consider if needed
+        public bool IsDeleted { get; set; }
+    }
+}
