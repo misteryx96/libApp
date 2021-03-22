4ba69b98a42db325350d58eaaa062671623aa1cd
misteryx96 <misteryx96@yahoo.com>
2021-03-22 03:19:26 +0100

    LibApp.Data Reservation

---
+++ LibApp/LibApp.Data/Model/Domain/Reservation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibApp.Data.Model.Domain
+{
+    public class Reservation
+    {
+        public int Id { get; set; }
+        public DateTime CreatedTimeStamp { get; set; }
+        public DateTime ModifiedTimestamp { get; set; }
+        public DateTime? DateExpiry { get; set; }
+        public DateTime? TookDateTime { get; set; }
+        public DateTime? ReturnDateTime { get; set; }
+        //TODO: UserId
+        public int UserId { get; set; }
+        public int LibrarianId { get; set; }
+    }
+}
