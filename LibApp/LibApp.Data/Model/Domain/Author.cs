73e13f1591e2bf2e3324f2852a04579cc436bf79
misteryx96 <misteryx96@yahoo.com>
2021-03-22 03:18:45 +0100

    LibApp.Data Author

---
+++ LibApp/LibApp.Data/Model/Domain/Author.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibApp.Data.Model.Domain
+{
+    public class Author
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime CreatedTimeStamp { get; set; }
+        public DateTime ModifiedTimestamp { get; set; }
+        //TODO: EF Joint table or not?
+    }
+}
