bb60ca3d9ce0efc9b33e01a9706f9e9b35bb8885
misteryx96 <misteryx96@yahoo.com>
2021-05-05 03:20:50 +0200

    LibApp.Data Department Modification

--- LibApp.Data/Model/Domain/Department.cs
+++ LibApp.Data/Model/Domain/Department.cs
@@ -8,9 +8,10 @@ namespace LibApp.Data.Model.Domain
         {
             Books = new HashSet<Book>();
         }
-
         public int Id { get; set; }
         public string Name { get; set; }
+        public DateTime CreatedTimeStamp { get; set; }
+        public DateTime ModifiedTimestamp { get; set; }
         public virtual ICollection<Book> Books { get; set; }
     }
 }
